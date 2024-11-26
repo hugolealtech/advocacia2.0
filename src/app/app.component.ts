710db400add66e471fd37d44708905c8c1278987
Hugo Leal <76668742+hugolealtech@users.noreply.github.com>
2024-11-25 21:48:22 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClientComponent } from './components/client/client.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MasterComponent } from './components/master/master.component';
+import { ReportsComponent } from './components/reports/reports.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { MatSlideToggleModule} from '@angular/material/slide-toggle'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    ClientComponent,
+    HomeComponent,
+    LoginComponent,
+    MasterComponent,
+    ReportsComponent,
+    SignupComponent,
+    MatSlideToggleModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'projeto-rotas';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
+import { ClientComponent } from './components/client/client.component';
+import { MasterComponent } from './components/master/master.component';
+import { ReportsComponent } from './components/reports/reports.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component:AppComponent},
+  {path:'login', component:LoginComponent},
+  {path:'signup', component:SignupComponent},
+  {path:'home', component:HomeComponent},
+  {path:'client', component:ClientComponent},
+  {path:'master',component:MasterComponent},
+  {path:'reports', component:ReportsComponent}
+
+];
